e7857f531d0b7868e35753210fe6a750d98cfd0b
Carlos Ramirez <c.ramirez@aqtiva.mx>
2020-08-03 16:43:45 -0500

    todo bien

--- clima/clima.js
+++ clima/clima.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 const getClima = async(lat, lng)=>{
 
-    const resp = await  axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=Your_key&units=metric`);
+    const resp = await  axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=d95acf208fcdf47a3c74c227f6effdcf&units=metric`);
     return resp.data.main.tempt;
 }
 
--- lugar/lugar.js
+++ lugar/lugar.js
@@ -8,25 +8,25 @@ const getLugarLatLng = async(dir)=>{
         baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${encodeUrl}`,
         headers: {
             "x-rapidapi-host": "devru-latitude-longitude-find-v1.p.rapidapi.com",
-            "x-rapidapi-key": "Your_key",
+            "x-rapidapi-key": "cd25f87c73mshe500f0bc9ed49f3p1431b4jsne2c3f1e13c69",
             "useQueryString": true
         }
     });
 
     const resp = await instance.get();
 
-    // if(resp.data.Results.length === 0){
-    //     throw new Error(`No hay resultados para ${dir}`);
-    // }
+    if(resp.data.Results.length === 0){
+        throw new Error(`No hay resultados para ${dir}`);
+    }
 
-    // const data = resp.data.Results[0];
-    // const direccion = data.name;
-    // const lat = data.lat;
-    // const lng = data.lon;
+    const data = resp.data.Results[0];
+    const direccion = data.name;
+    const lat = data.lat;
+    const lng = data.lon;
 
-    const direccion = 'Tawarano';
-    const lat = 35;
-    const lng = 139;
+    // const direccion = 'Tawarano';
+    // const lat = 35;
+    // const lng = 139;
 
     return {
         direccion,
